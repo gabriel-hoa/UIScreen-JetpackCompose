4b92fdbfe3b8c93a47cffd91e603825a253af2b1
gabriel-hoa <gabrielhoaraujo100@gmail.com>
2024-02-08 23:45:16 -0300

    Add Color, Theme and Type

--- app/src/main/java/com/example/ticketbookingscreen/ui/theme/Type.kt
+++ app/src/main/java/com/example/ticketbookingscreen/ui/theme/Type.kt
@@ -1,34 +1,44 @@
 package com.example.ticketbookingscreen.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
+val RedHat = FontFamily(
+    listOf(
+        font(R.font.red_hot_display_regular, FontWeight.Normal),
+        font(R.font.red_hot_display_medium, FontWeight.Medium),
+        font(R.font.red_hot_display_semibold, FontWeight.SemiBold),
+        font(R.font.red_hot_display_bold, FontWeight.Bold),
+        )
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+   titleSmall = TextStyle(
+       color = Color.White,
+       fontSize = 17.sp,
+       fontFamily = RedHat,
+       fontWeight = FontWeight.SemiBold
+   ),
+   labelSmall = TextStyle(
+       color = Color.White,
+       fontSize = 14.sp,
+       fontFamily = RedHat,
+       fontWeight = FontWeight.Bold
+   ),
+   labelMedium = TextStyle(
+       color = Color.White,
+       fontSize = 17.sp,
+       fontFamily = RedHat,
+       fontWeight = FontWeight.SemiBold
+   ),
+   labelLarge = TextStyle(
+       color = Yellow,
+       fontSize = 29.sp,
+       fontFamily = RedHat,
+       fontWeight = FontWeight.SemiBold
+   )
 )
